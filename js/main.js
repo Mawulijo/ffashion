c4bd180866ff0b6be85c394ab8b89038b0843645
Joshua M. Agbeku <mawulidickson@gmail.com>
2017-05-12 19:21:22 +0000

    First commit on first fashion

---
+++ js/main.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $(".added").hide();
+	
+	$("#1").click(function () {      
+        $("#8").fadeIn("slow"); 
+        $("#8").fadeOut("slow");
+		}) 
+    $("#2").click(function () {      
+        $("#9").fadeIn("slow"); 
+        $("#9").fadeOut("slow");
+		}) 
+    $("#3").click(function () {      
+        $("#10").fadeIn("slow"); 
+        $("#10").fadeOut("slow");
+		})
+    $("#4").click(function () {      
+        $("#11").fadeIn("slow"); 
+        $("#11").fadeOut("slow");
+		})
+    $("#5").click(function () {      
+        $("#12").fadeIn("slow"); 
+        $("#12").fadeOut("slow");
+		})
+    $("#6").click(function () {      
+        $("#13").fadeIn("slow"); 
+        $("#13").fadeOut("slow");
+		})
+}); //ready
